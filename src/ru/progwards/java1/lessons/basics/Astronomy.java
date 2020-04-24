1319a9fd4e0f51a4ed80a170647670d54e81116e
Bigetti <upgrade81@mail.ru>
2020-04-24 22:34:28 +0300

    earth

--- src/ru/progwards/java1/lessons/basics/Astronomy.java
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -2,19 +2,29 @@ package ru.progwards.java1.lessons.basics;
 
 public class Astronomy {
 
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         sphereSquare(2.2);
-
+        earthSquare();
     }
 
 
-    public static Double sphereSquare(Double r){
+    public static Double sphereSquare(Double r)
+    {
         double p = 3.14;
         //double r;
         double S = 4*p*r*r;
 
         System.out.println(S);
         return S;
+    }
+
+
+    public static Double earthSquare()
+    {
+        double Earth = sphereSquare(6371.2);
+        System.out.println(Earth);
+        return Earth;
     }
 
 
