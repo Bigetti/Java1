d9b49cf45c4629546b93022ec8f13ad3a90caa69
Bigetti <upgrade81@mail.ru>
2020-04-30 22:51:13 +0300

    fibona

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,40 +1,39 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class CyclesGoldenFibo {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
+
         System.out.println(1 + " -это 1-е по счету число Фибоначчи");
         System.out.println(1 + " -это 2-е по счету число Фибоначчи");
-        for (int i = 1; i<14; i++)
+        for (int i = 1; i < 14; i++)
         {
             int x = fiboNumber(i);
         }
 
         fiboNumber(10);
-        //boolean tr = false;
+
+
+
         int a;
         int b;
         int c;
-                for (a = 1; a < 101; a++) {
-                    for (b = 1; b < 101; b++) {
-                        for (c = 1; c < 101; c++) {
+        for (a = 1; a < 101; a++) {
+            for (b = 1; b < 101; b++) {
+                for (c = 1; c < 101; c++) {
 
-                            isGoldenTriangle(a, b, c);
+                    isGoldenTriangle(a, b, c);
 
-                        }
-                    }
                 }
+            }
+        }
+
+        containsDigit(81567657, 6);
     }
 
 
-/*
-       public static boolean containsDigit(int number, int digit)
-       {
-
-       }
-   */
-    public static int fiboNumber(int n)
-    {
+    public static int fiboNumber(int n) {
         int a = 1;
         int b = 1;
 
@@ -45,37 +44,52 @@ public class CyclesGoldenFibo {
             a = b;
             b = sum_fib;
 
-            }
+        }
         System.out.print(sum_fib);
-        System.out.println(" -Число Фибоначчи " + (n+2) + "по счету");
+        System.out.println(" -Число Фибоначчи " + (n + 2) + "по счету");
         return sum_fib;
     }
 
-    public static boolean isGoldenTriangle(int a, int b, int c)
-    {
+    public static boolean isGoldenTriangle(int a, int b, int c) {
         final float aa = a;
         final float bb = b;
 
 
-        if (a == b && (aa / c) > 1.61703 && (aa / c) < 1.61903)
-
-        {
+        if (a == b && (aa / c) > 1.61703 && (aa / c) < 1.61903) {
             System.out.println("есть! Стороны одного из золотых треугольников = " + a + " " + b + " " + c);
             return true;
-        }
-        else if (a == c && (aa / b) > 1.61703 && (aa / b) < 1.61903)
-        {
+        } else if (a == c && (aa / b) > 1.61703 && (aa / b) < 1.61903) {
             System.out.println("есть! Стороны одного из золотых треугольников = " + a + " " + b + " " + c);
             return true;
-        }
-        else if (b == c && (bb / a) > 1.61703 && (bb / a) < 1.61903)
-        {
+        } else if (b == c && (bb / a) > 1.61703 && (bb / a) < 1.61903) {
             System.out.println("есть! Стороны одного из золотых треугольников = " + a + " " + b + " " + c);
             return true;
-        }
-        else
-        {
+        } else {
             return false;
         }
     }
+
+    public static boolean containsDigit(int number, int digit)
+    {
+        System.out.println("Ищем цифру " + digit + " в числе " + number);
+        int celoe = number;
+        int b = 10;
+
+        do {
+            int zzz = celoe;
+            int ostatok = zzz % 10;
+            if (ostatok == digit) {
+                System.out.println(" цифра " + ostatok + " есть в числе " + number);
+                return true;
+            } else {
+                System.out.println("Пока ищем далее");
+                   }
+            celoe = celoe/b;
+            }
+        while (celoe >= 1);
+
+        System.out.println("Net Sovpadeny " + false);
+            return false;
+    }
 }
+
