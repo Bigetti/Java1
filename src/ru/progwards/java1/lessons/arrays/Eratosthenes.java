b89d8f8c7ffabc30f5a238269fbee407e47974f7
Bigetti <upgrade81@mail.ru>
2020-06-04 18:19:57 +0300

    Restangle

---
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -0,0 +1,34 @@
+package ru.progwards.java1.lessons.arrays;
+
+import java.util.Arrays;
+
+public class Eratosthenes {
+
+    private boolean[] sieve;
+
+
+    //////////////////////////////////////////////////
+    public static void main(String[] args) {
+
+    }
+    //////////////////////////////////////////////////
+
+
+    public Eratosthenes(int N) {
+        this.sieve = sift();
+
+
+    }
+
+    private boolean[] sift() {
+        Arrays.fill(sieve, true);
+        for (int i = 0; i < N; i++)
+
+
+            public boolean isSimple ( int n){
+
+        }
+
+
+    }
+}
