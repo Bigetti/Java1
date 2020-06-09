248884df5070b81658ddc1664bae70d5f771bf40
Bigetti <upgrade81@mail.ru>
2020-06-09 17:18:00 +0300

    array.DIntArray

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -6,24 +6,30 @@ public class DIntArray {
 
     private int[] mass1;
     private int[] mass2;
-    private int num = 111;
+    int num = 44;
 
 
 
 
     //////////////////////////////////////////////////////
     public static void main(String[] args) {
+
         int[] mass1 = new int[]{4, 6, 77, 22 ,-1};
         int[] mass2 = Arrays.copyOf(mass1,mass1.length + 1);
         System.out.println(Arrays.toString(mass1));
         System.out.println(Arrays.toString(mass2));
         System.out.println(mass1.length);
         System.out.println(mass2.length);
+        int num = 44;
+        mass2[5] = num;
+        System.out.println(Arrays.toString(mass2));
+        System.out.println(num);
+
 
  //       mass2[mass2.length]  = 8;
 
-        System.out.println(Arrays.toString(mass1));
-        System.out.println(Arrays.toString(mass2));
+        //System.out.println(Arrays.toString(mass1));
+        //System.out.println(Arrays.toString(mass2));
 
 
 
@@ -33,13 +39,10 @@ public class DIntArray {
     public DIntArray(int[] mass1){
 
         this.mass1 = mass1;
-        //this.mass2 = mass2;
-    }
+
+        }
 
 
-    public void add() {
-        add();
-    }
 
     public void add(int num){
         mass2[mass2.length]  = num;
