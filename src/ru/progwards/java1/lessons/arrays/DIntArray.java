23e713f30cb8c622b797620c2d957c699108eb39
Bigetti <upgrade81@mail.ru>
2020-06-12 18:26:30 +0300

    array.DIntArray

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -6,7 +6,7 @@ public class DIntArray {
 
     private int[] mass1 = {4, 6, 77, 22 ,-1};
 
-    static int num = 44;
+    static int num;
     static int pos;
 
     //////////////////////////////////////////////////////
@@ -16,18 +16,18 @@ public class DIntArray {
 
         System.out.println("Длина массива первоначальная " + mass.mass1.length);
         System.out.println("Первоначальный массив " + Arrays.toString(mass.mass1));
-        mass.add(num);
+        mass.add(55);
         System.out.println("Массив после добавления элемента нум " + Arrays.toString(mass.mass1));
 
         System.out.println("Длина массива после добавления элемента нум " + mass.mass1.length);
 
-        mass.atInsert(pos,num);
+        mass.atInsert(4,77);
         System.out.println("Длина массива после добавления элемента нум в позицию пос " + mass.mass1.length);
         System.out.println("Массив после добавления элемента нум в позицию ПОС " + Arrays.toString(mass.mass1));
         //System.out.println(" Номер элемента ПОС " + pos);
         //System.out.println("Значение элемента ПОС " + mass.mass1[pos]);
-        mass.atDelete(pos);
-        mass.at(2);
+        mass.atDelete(2);
+        mass.at(5);
 
 
     }
@@ -50,7 +50,7 @@ public class DIntArray {
     //
     public void atInsert(int pos, int num){
         int[] mass3 = Arrays.copyOf(mass1,mass1.length + 1);
-        pos = (mass3.length)/2;
+        //pos = (mass3.length)/2;
 
         System.out.println("Номер элемента ПОС " + pos);
         mass3[mass3.length - 1 ] = mass3[pos];
@@ -62,7 +62,7 @@ public class DIntArray {
 
     public void atDelete(int pos){
 
-        pos = mass1.length/2;
+        //pos = mass1.length/2;
         mass1[pos] = mass1[mass1.length - 1];
         int[] mass4 = Arrays.copyOf(mass1, mass1.length - 1);
         mass1 = mass4;
