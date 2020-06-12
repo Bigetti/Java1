dc4521867c642fa997d5c874853918e9c6d02255
Bigetti <upgrade81@mail.ru>
2020-06-12 16:54:31 +0300

    array.DIntArray

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -15,6 +15,7 @@ public class DIntArray {
         DIntArray mass = new DIntArray();
 
         System.out.println("Длина массива первоначальная " + mass.mass1.length);
+        System.out.println("Первоначальный массив " + Arrays.toString(mass.mass1));
         mass.add(num);
         System.out.println("Массив после добавления элемента нум " + Arrays.toString(mass.mass1));
 
@@ -25,6 +26,8 @@ public class DIntArray {
         System.out.println("Массив после добавления элемента нум в позицию ПОС " + Arrays.toString(mass.mass1));
         //System.out.println(" Номер элемента ПОС " + pos);
         //System.out.println("Значение элемента ПОС " + mass.mass1[pos]);
+        mass.atDelete(pos);
+        mass.at(2);
 
 
     }
@@ -46,9 +49,9 @@ public class DIntArray {
 
     //
     public void atInsert(int pos, int num){
-    int[] mass3 = Arrays.copyOf(mass1,mass1.length + 1);
+        int[] mass3 = Arrays.copyOf(mass1,mass1.length + 1);
         pos = (mass3.length)/2;
-        //System.out.println(mass3.length);
+
         System.out.println("Номер элемента ПОС " + pos);
         mass3[mass3.length - 1 ] = mass3[pos];
         mass3[pos] = num;
@@ -56,13 +59,24 @@ public class DIntArray {
         mass1 = mass3;
 
     }
-//
-//    public void atDelete(int pos){
-//
-//    }
-//
-//    public int at(int pos){
-//      return ;
-//    }
+
+    public void atDelete(int pos){
+
+        pos = mass1.length/2;
+        mass1[pos] = mass1[mass1.length - 1];
+        int[] mass4 = Arrays.copyOf(mass1, mass1.length - 1);
+        mass1 = mass4;
+        //System.out.println(pos);
+        //System.out.println(mass1[pos]);
+        //System.out.println(Arrays.toString(mass1));
+        System.out.println("Массив после удаления элемента в позиции ПОС " + Arrays.toString(mass4));
+    }
+
+    public int at(int pos){
+
+        System.out.println("Значение элемента в индексе ПОС = " + mass1[pos]);
+        return mass1[pos];
+
+    }
 
 }
